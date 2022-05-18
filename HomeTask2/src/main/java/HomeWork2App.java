1763103f1ffc0559d81863d00c73aa44681936a6
Maksim <krimaxim2010@yandex.ru>
2022-05-19 00:15:39 +0300

    4 from 5 tasks done...

--- HomeTask2/src/main/java/HomeWork2App.java
+++ HomeTask2/src/main/java/HomeWork2App.java
@@ -1,10 +1,14 @@
 public class HomeWork2App {
     public static void main(String[] args) {
         CompareTwoNumbers(13,4);
+        //System.out.println(CompareTwoNumbers(13,4)); this string is just for check method CompareTwoNumbers works good
 
-        System.out.println(CompareTwoNumbers(3,4));
         DetectSign(-5);
-        DetectSign2();
+
+        DetectSign2(8);
+        //System.out.println(DetectSign2(8)); this string is just for check method DetectSign2 works good
+        
+        PrintStringNTimes("i like java",5);
     }
 
     private static boolean CompareTwoNumbers(int a, int b) {
@@ -18,7 +22,7 @@ public class HomeWork2App {
 
     private static void DetectSign(int a) {
         if (a >= 0) {
-            System.out.println("Nubmer is positive");
+            System.out.println("Number is positive");
         }
         else System.out.println("Number is negative");
     }
@@ -29,5 +33,11 @@ public class HomeWork2App {
             return (false);
         }
         return(true);
+    }
+
+    private static void PrintStringNTimes(String str, int N) {
+        for (int i = 0; i < N; i++) {
+            System.out.println(str);
+        }
     }
 }
