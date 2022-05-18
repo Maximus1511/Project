fdea39d68f98a0cc6061fbbab06761656c48583e
Maksim <krimaxim2010@yandex.ru>
2022-05-19 00:48:19 +0300

    ex.5 done, but i will try to find better way

--- HomeTask2/src/main/java/HomeWork2App.java
+++ HomeTask2/src/main/java/HomeWork2App.java
@@ -1,14 +1,21 @@
 public class HomeWork2App {
     public static void main(String[] args) {
-        CompareTwoNumbers(13,4);
+       // CompareTwoNumbers(13,4);
         //System.out.println(CompareTwoNumbers(13,4)); this string is just for check method CompareTwoNumbers works good
 
-        DetectSign(-5);
+        //DetectSign(-5);
 
         DetectSign2(8);
         //System.out.println(DetectSign2(8)); this string is just for check method DetectSign2 works good
-        
-        PrintStringNTimes("i like java",5);
+
+        //PrintStringNTimes("i like java",5);
+
+
+        IsLeapYear(1600);
+        IsLeapYear(1900);
+        IsLeapYear(1904);
+        IsLeapYear(2000);
+        //System.out.println(IsLeapYear(2020)); //this string is just for check method IsLeapYear works good
     }
 
     private static boolean CompareTwoNumbers(int a, int b) {
@@ -39,5 +46,17 @@ public class HomeWork2App {
         for (int i = 0; i < N; i++) {
             System.out.println(str);
         }
+    }
+
+    private static void IsLeapYear(int year) {
+        if (year % 4 == 0)   {
+            if (year % 400 == 0){
+                System.out.println("Year " + year + " is leap");
+            } else if (year % 100 == 0) {
+                System.out.println("Year " + year + " is NOT leap");
+                    }
+            else System.out.println("Year " + year + " is leap");
+        } else System.out.println("Year " + year + " is NOT leap");
+
     }
 }
